1ef1970f40d4ac3b49e1e2f8f39e9053dfe11fc0
Hyeyeon-Ko <khy33355@gmail.com>
2024-01-02 12:24:36 +0900

    Splash, Intro 레이아웃 구현

--- app/src/main/java/com/example/mysololife/auth/IntroActivity.kt
+++ app/src/main/java/com/example/mysololife/auth/IntroActivity.kt
@@ -1,13 +1,15 @@
-package com.example.mysololife
+package com.example.mysololife.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mysololife.R
 
-class MainActivity : AppCompatActivity() {
+class IntroActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_intro)
 
     }
 }

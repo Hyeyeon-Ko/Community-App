4190595115ec140840034aecb25f913807fa28b4
Hyeyeon-Ko <khy33355@gmail.com>
2024-01-05 17:08:17 +0900

    메인화면 레이아웃 구현 - Navigation

---
+++ app/src/main/java/com/example/mysololife/fragments/StoreFragment.kt
@@ -0,0 +1,28 @@
+package com.example.mysololife.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.mysololife.R
+import com.example.mysololife.databinding.FragmentHomeBinding
+import com.example.mysololife.databinding.FragmentStoreBinding
+
+class StoreFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_store, container, false)
+
+    }
+}
